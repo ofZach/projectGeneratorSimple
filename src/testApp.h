843fe3ee60c83802c5d2e6b36b99228ff4d67b20
LeoColomb <leocolombaro@yahoo.fr>
2013-03-26 18:54:06 +0100

    Tabify

--- src/testApp.h
+++ src/testApp.h
@@ -33,57 +33,57 @@ class testApp : public ofBaseApp{
 		void dragEvent(ofDragInfo dragInfo);
 		void gotMessage(ofMessage msg);
  
-    
-        string sketchName;
-        string sketchPath;
-        string addons;
-        string platform;
-    
-        ofxPanel panelCoreAddons;
-        ofxPanel panelOtherAddons;
-        bool bHaveNonCoreAddons;
-    
-        ofxPanel panelPlatforms;
-    
-        ofxToggle osxToggle, iosToggle, wincbToggle, winvsToggle, linuxcbToggle, linux64cbToggle;
+	
+		string sketchName;
+		string sketchPath;
+		string addons;
+		string platform;
+	
+		ofxPanel panelCoreAddons;
+		ofxPanel panelOtherAddons;
+		bool bHaveNonCoreAddons;
+	
+		ofxPanel panelPlatforms;
+	
+		ofxToggle osxToggle, iosToggle, wincbToggle, winvsToggle, linuxcbToggle, linux64cbToggle;
 
-        ofTrueTypeFont font;
-        ofTrueTypeFont titleFont;
-        ofTrueTypeFont secondFont;
-    
-        int mode;
-        enum { MODE_NORMAL, MODE_ADDON, MODE_PLATFORM };
-    
-        baseProject * project;
-    
-        string setupForTarget(int targ);
-    
-        void generateProject();
-    
-        string addonsPath;
-        string status;
-    
-        ofxXmlSettings XML;
-        string appToRoot;
-        string defaultLoc;
+		ofTrueTypeFont font;
+		ofTrueTypeFont titleFont;
+		ofTrueTypeFont secondFont;
+	
+		int mode;
+		enum { MODE_NORMAL, MODE_ADDON, MODE_PLATFORM };
+	
+		baseProject * project;
+	
+		string setupForTarget(int targ);
+	
+		void generateProject();
+	
+		string addonsPath;
+		string status;
+	
+		ofxXmlSettings XML;
+		string appToRoot;
+		string defaultLoc;
 		
 		float uiLeftX; 
-    
-    
-        textButton  button;
-        textButton  generateButton;
-        textButton  addonButton;
-        vector < textButton > buttons;
+	
+	
+		textButton  button;
+		textButton  generateButton;
+		textButton  addonButton;
+		vector < textButton > buttons;
 
-        bool isAddonCore(string addon);
-        bool bInited;
-        vector < string  > coreAddons;
-    
-        
-        float statusSetTime;
-        float statusEnergy;
-        void setStatus(string newStatus);
-        
-        ofImage logo;
-    
+		bool isAddonCore(string addon);
+		bool bInited;
+		vector < string  > coreAddons;
+	
+		
+		float statusSetTime;
+		float statusEnergy;
+		void setStatus(string newStatus);
+		
+		ofImage logo;
+	
 };
