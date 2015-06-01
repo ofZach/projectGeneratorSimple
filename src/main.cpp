9a5af8a12bd07a2282d572e5385e82194e6f93fc
Ingo Randolf <ingo.randolf@servus.at>
2015-06-01 10:43:22 +0200

    use standard way to create the window

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,9 @@
 #include "ofMain.h"
 #include "testApp.h"
-#include "ofAppGLFWWindow.h"
-#include "ofAppNoWindow.h"
-
 
 //========================================================================
 int main(  int argc, char *argv[]  ){
 	
-	ofGLFWWindowSettings settings;
-	settings.width = 1024;
-	settings.height = 610;
-	settings.setPosition(ofVec2f(300,0));
-	settings.resizable = false;
-	shared_ptr<ofAppBaseWindow> mainWindow = ofCreateWindow(settings);
-	mainWindow->setVerticalSync(true);
-	
-	shared_ptr<testApp> mainApp(new testApp);
-	
-	ofRunApp(mainWindow, mainApp);
-	ofRunMainLoop();
-
+	ofSetupOpenGL(1024,610,OF_WINDOW);
+	ofRunApp(new testApp());
 }
